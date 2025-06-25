86b59884aa24546540076c10e7174e35195dec13
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-06-25 11:38:59 +0530

    [Clean] Removed all files from main branch

--- Java Programing Elemetns/d1new3.java
+++
@@ -1,7 +0,0 @@
-class d1_programingElement{
-	public static void main(String args[]){
-		double distance=10.8;
-		double distanceInMiles=distance*1.6;
-		System.out.print("The distance "+distance+" km in miles is "+distanceInMiles);
-	}
-}
