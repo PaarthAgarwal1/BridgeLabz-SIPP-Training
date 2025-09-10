6322fc7aa7812534630727fb83a25991a7da6307
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-09-10 09:35:05 +0530

    [Paarth] Add. codes of Linear and Binary Search

---
+++ javaSearchingAlgo/FirstNegative.java
@@ -0,0 +1,10 @@
+package javaSearchingAlgo;
+
+public class FirstNegative {
+    public static int findFirstNegative(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) return i;
+        }
+        return -1;
+    }
+}
---
+++ javaSearchingAlgo/SearchWordInSentences.java
@@ -0,0 +1,10 @@
+package javaSearchingAlgo;
+
+public class SearchWordInSentences {
+    public static String search(String[] sentences, String word) {
+        for (String s : sentences) {
+            if (s.contains(word)) return s;
+        }
+        return "Not Found";
+    }
+}
